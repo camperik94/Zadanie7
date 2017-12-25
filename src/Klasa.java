cb95c8b6be76b96c424b1650916768e94558147e
camperik94 <tomaszjach94@gmail.com>
2017-12-25 13:19:52 +0100

    Dodanie funkcjonalnosci do programu

--- src/Klasa.java
+++ src/Klasa.java
@@ -1,4 +1,24 @@
 
 public class Klasa {
-
+	private int number;
+	
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	
+	public Klasa(int number) {
+		this.number = number;
+	}
+	
+	public void metoda() {
+		for(int i = 0; i <= number; i++) {
+			if(i % 2 == 1) {
+				System.out.println(i);
+			}
+		}
+	}
 }
--- src/Zadanie.java
+++ src/Zadanie.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
 
 public class Zadanie {
 	public static void main(String[] args) {
+		System.out.println("Podaj liczbe naturalna wieksza od zera");
 		
+		Scanner in = new Scanner(System.in);
+		
+		int liczba = in.nextInt();
+		
+		Klasa klas = new Klasa(liczba);
+		
+		klas.metoda();
 	}
 }
